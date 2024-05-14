54402addd70cea13f317ca6b1b9794412e25f1f7
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-05-15 01:39:15 +0800

    added hourly forecast

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -3,13 +3,29 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 interface WeatherData {
-  location: { name: string };
+  location: { name: string,
+              localtime: string};
   current: {
     temp_c: number;
+    is_day: number;
     condition: { text: string; icon: string };
+    wind_mph: number;
     humidity: number;
-    feelslike_c: number;
   };
+
+  forecast: {
+    forecastday: ForecastDay[]; 
+  };
+}
+
+interface ForecastDay {
+  hour: HourData[]; 
+}
+
+interface HourData {
+  time: string;
+  temp_c: number;
+  condition: { text: string };
 }
 
 @Injectable({
@@ -18,7 +34,7 @@ interface WeatherData {
 export class WeatherService {
 
   private apiKey = '30c849f741a64b5898382548241405';
-  private apiUrl = 'http://api.weatherapi.com/v1/current.json';
+  private apiUrl = 'http://api.weatherapi.com/v1/forecast.json';
 
   constructor(private http: HttpClient) {}
 
@@ -26,5 +42,4 @@ export class WeatherService {
     const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}`;
     return this.http.get<WeatherData>(url);
   }
-
 }
