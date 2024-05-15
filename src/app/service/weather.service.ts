0114dca9025031527c06cab1577636a348cf05d7
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-05-15 17:21:58 +0800

    added 7 day forecast

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 interface WeatherData {
+
+  //curent day forecast
   location: { name: string,
               localtime: string};
   current: {
@@ -18,14 +21,29 @@ interface WeatherData {
   };
 }
 
+// Forecast for the next 7 days
 interface ForecastDay {
+  date: string; 
+  day: {
+    maxtemp_c: number;
+    mintemp_c: number;
+    condition: {
+      text: string;
+      icon: string;
+    };
+  };
   hour: HourData[]; 
 }
 
+//hourly forecast for the next 7 days since the api also returns for the next 7 days
 interface HourData {
   time: string;
-  temp_c: number;
-  condition: { text: string };
+  maxtemp_c: number;
+  mintemp_c: number;
+  condition: { 
+    text: string;
+    icon: string;
+  };
 }
 
 @Injectable({
@@ -35,11 +53,12 @@ export class WeatherService {
 
   private apiKey = '30c849f741a64b5898382548241405';
   private apiUrl = 'http://api.weatherapi.com/v1/forecast.json';
+  private days = '7';
 
   constructor(private http: HttpClient) {}
 
   getWeather(city: string): Observable<WeatherData> {
-    const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}`;
+    const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}&days=${this.days}`;
     return this.http.get<WeatherData>(url);
   }
 }
