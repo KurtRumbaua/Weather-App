8f27db6c5357df800a9b20363b4e91aaca417bec
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-05-16 17:37:27 +0800

    added today/weekly toggle button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../../service/weather.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card'
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonToggleModule,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
 
-  weatherData: any; // Initialize weatherData
+  selectedForecast = 'hourly'; // Default to showing the hourly forecast
+  weatherData: any;
 
   constructor(private weatherService: WeatherService) {}
 
+  //function that calls GET function from the service and then passes city name
   ngOnInit(): void {
       this.weatherService.getWeather('Manila').subscribe(data => {
       this.weatherData = data;
