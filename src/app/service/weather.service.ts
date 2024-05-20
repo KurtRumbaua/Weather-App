fbaba194d404e56b43a18cf377f627b1eb086b60
KurtRumbaua <kurtrumbaua28@gmail.com>
2024-05-20 17:10:02 +0800

    added today's highlights

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -6,14 +6,24 @@ import { Observable } from 'rxjs';
 interface WeatherData {
 
   //curent day forecast
-  location: { name: string,
-              localtime: string};
+  location: { 
+    name: string,
+    localtime: string
+  };
+
   current: {
     temp_c: number;
     is_day: number;
     condition: { text: string; icon: string };
-    wind_mph: number;
+    wind_kph: number;
+    wind_dir: string;
     humidity: number;
+    vis_km: number;
+    uv: number; 
+    air_quality:{
+      co: number;
+    }
+
   };
 
   forecast: {
@@ -30,8 +40,12 @@ interface ForecastDay {
     condition: {
       text: string;
       icon: string;
-    };
+    }; 
   };
+  astro: {
+    sunrise: string;
+    sunset: string;
+  }
   hour: HourData[]; 
 }
 
@@ -51,14 +65,17 @@ interface HourData {
 })
 export class WeatherService {
 
+  
   private apiKey = '30c849f741a64b5898382548241405';
   private apiUrl = 'http://api.weatherapi.com/v1/forecast.json';
   private days = '7';
+  private aqi = 'yes';
 
   constructor(private http: HttpClient) {}
 
+  //GET function for api json data, sets "city" as a parameter
   getWeather(city: string): Observable<WeatherData> {
-    const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}&days=${this.days}`;
+    const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}&days=${this.days}&aqi=${this.aqi}`;
     return this.http.get<WeatherData>(url);
   }
 }
